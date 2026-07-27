c4b43000c4558cd796e3b15ebdcb94cc77473014
KhatishadbyTakia <jebatakia8@gmail.com>
2026-07-27 23:55:01 +0600

    Add files via upload

---
+++ category.js
@@ -0,0 +1,686 @@
+/* ==========================================
+   Khati Shad by Takia
+   category.js
+   Part-1
+========================================== */
+
+// =======================
+// URL Parameter
+// =======================
+
+const params = new URLSearchParams(window.location.search);
+
+const currentCategory = params.get("category") || "";
+
+
+// =======================
+// DOM Elements
+// =======================
+
+const productContainer = document.getElementById("productContainer");
+const categoryTitle = document.getElementById("categoryTitle");
+
+
+// =======================
+// Category Names
+// =======================
+
+const categoryNames = {
+
+    honey: "আমাদের সকল মধু",
+
+    ghee: "আমাদের সকল ঘি",
+
+    tel: "আমাদের সকল তেল",
+
+    spices: "আমাদের সকল মসলা",
+
+    achar: "আমাদের সকল আচার"
+
+};
+
+
+// =======================
+// Change Heading
+// =======================
+
+if (categoryTitle) {
+
+    categoryTitle.textContent =
+        categoryNames[currentCategory] ||
+        "আমাদের সকল পণ্য";
+
+}
+
+
+// =======================
+// Filter Products
+// =======================
+
+const filteredProducts = products.filter(product => {
+
+    return product.category === currentCategory;
+
+});
+
+
+// =======================
+// Format Price
+// =======================
+
+function formatPrice(price){
+
+    return "৳ " + Number(price);
+
+}
+
+
+// =======================
+// Stock Badge
+// =======================
+
+function getStockBadge(product){
+
+    if(product.stock){
+
+        return `
+        <span class="stock-badge">
+            IN STOCK
+        </span>
+        `;
+
+    }
+
+    return `
+    <span class="stock-badge bg-danger">
+        OUT OF STOCK
+    </span>
+    `;
+
+}
+/* ==========================================
+   CREATE PRODUCT CARD
+   Part-2
+========================================== */
+
+
+function createProductCard(product, index){
+
+
+    let firstSize = product.sizes[0];
+
+
+
+    let sizeButtons = "";
+
+
+
+    product.sizes.forEach((size, i)=>{
+
+
+        sizeButtons += `
+
+        <button 
+        class="size-btn ${i===0 ? "active":""}"
+
+        onclick="changeCategorySize(
+        ${product.id},
+        ${size.price},
+        '${size.weight}',
+        this
+        )">
+
+            ${size.label}
+
+        </button>
+
+        `;
+
+
+    });
+
+
+
+
+
+    return `
+
+
+    <div class="col-xl-4 col-lg-4 col-md-4 col-6 product-item">
+
+
+    <div class="card product-card h-100"
+    style="position:relative;">
+
+
+
+    ${getStockBadge(product)}
+
+
+
+    <img src="${product.image}"
+
+    class="card-img-top"
+
+    alt="${product.name}">
+
+
+
+
+
+    <div class="card-body">
+
+
+
+
+
+    <h5 class="product-name">
+
+        ${product.name}
+
+    </h5>
+
+
+
+
+
+    <h3 
+    id="price${product.id}"
+
+    class="product-price">
+
+        ৳ ${firstSize.price}
+
+    </h3>
+
+
+
+
+
+    <small
+
+    id="weight${product.id}"
+
+    class="text-muted d-block mb-3">
+
+        ${firstSize.weight}
+
+    </small>
+
+
+
+
+
+
+    <div class="size-selector">
+
+
+        ${sizeButtons}
+
+
+    </div>
+
+
+
+
+
+
+
+    <div class="quantity-box">
+
+
+
+        <button onclick="minusCategoryQty(${product.id})">
+
+            <i class="fa-solid fa-minus"></i>
+
+        </button>
+
+
+
+
+
+        <input
+
+        id="qty${product.id}"
+
+        type="text"
+
+        value="1"
+
+        readonly>
+
+
+
+
+
+
+
+        <button onclick="plusCategoryQty(${product.id})">
+
+            <i class="fa-solid fa-plus"></i>
+
+        </button>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+    <button
+
+    class="btn btn-success w-100 mt-3"
+
+
+    onclick="addCategoryCart(
+
+    '${product.name}',
+
+    '${product.image}',
+
+    ${product.id}
+
+    )">
+
+
+    <i class="fa-solid fa-cart-plus"></i>
+
+    কার্টে যোগ করুন
+
+
+    </button>
+
+
+
+
+
+
+    </div>
+
+
+    </div>
+
+
+    </div>
+
+
+    `;
+
+
+}
+/* ==========================================
+   CATEGORY PRODUCT FUNCTIONS
+   Part-3
+========================================== */
+
+
+
+// =======================
+// Render Products
+// =======================
+
+
+function renderProducts(){
+
+
+    if(!productContainer)
+        return;
+
+
+
+    productContainer.innerHTML = "";
+
+
+
+    if(filteredProducts.length === 0){
+
+
+        productContainer.innerHTML = `
+
+
+        <div class="col-12">
+
+
+            <div class="alert alert-warning text-center">
+
+
+                এই ক্যাটাগরিতে কোন পণ্য নেই।
+
+
+            </div>
+
+
+        </div>
+
+
+        `;
+
+
+        return;
+
+    }
+
+
+
+
+
+    filteredProducts.forEach((product,index)=>{
+
+
+        productContainer.innerHTML +=
+
+        createProductCard(product,index);
+
+
+    });
+
+
+
+}
+
+
+
+
+
+// =======================
+// Selected Category Data
+// =======================
+
+
+const categorySelected = {};
+
+
+
+
+
+// =======================
+// Change Size
+// =======================
+
+
+function changeCategorySize(
+
+id,
+
+price,
+
+weight,
+
+button
+
+){
+
+
+
+    categorySelected[id] = {
+
+
+        price:Number(price),
+
+        weight:weight
+
+
+    };
+
+
+
+
+
+    const priceBox = 
+
+    document.getElementById(
+        "price"+id
+    );
+
+
+
+    const weightBox =
+
+    document.getElementById(
+        "weight"+id
+    );
+
+
+
+
+
+    if(priceBox){
+
+        priceBox.innerHTML =
+        "৳ " + price;
+
+    }
+
+
+
+
+
+    if(weightBox){
+
+        weightBox.innerHTML =
+        weight;
+
+    }
+
+
+
+
+
+
+    if(button){
+
+
+
+        button.parentElement
+
+        .querySelectorAll(".size-btn")
+
+        .forEach(btn=>{
+
+
+            btn.classList.remove(
+                "active"
+            );
+
+
+        });
+
+
+
+
+        button.classList.add(
+            "active"
+        );
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+// =======================
+// Quantity Plus
+// =======================
+
+
+function plusCategoryQty(id){
+
+
+    const qty =
+
+    document.getElementById(
+        "qty"+id
+    );
+
+
+
+    if(!qty)
+        return;
+
+
+
+    qty.value =
+    Number(qty.value)+1;
+
+
+
+}
+
+
+
+
+
+
+
+// =======================
+// Quantity Minus
+// =======================
+
+
+function minusCategoryQty(id){
+
+
+    const qty =
+
+    document.getElementById(
+        "qty"+id
+    );
+
+
+
+    if(!qty)
+        return;
+
+
+
+
+    if(Number(qty.value)>1){
+
+
+        qty.value =
+        Number(qty.value)-1;
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+// =======================
+// Add Category Cart
+// =======================
+
+
+function addCategoryCart(
+
+name,
+
+image,
+
+id
+
+){
+
+
+
+    const qty =
+
+    Number(
+
+    document.getElementById(
+        "qty"+id
+    ).value
+
+    );
+
+
+
+
+
+    if(!categorySelected[id]){
+
+
+
+        showToast(
+        "⚠️ আগে ওজন নির্বাচন করুন"
+        );
+
+
+        return;
+
+
+    }
+
+
+
+
+
+    addToCart(
+
+
+
+        name,
+
+
+        categorySelected[id].price,
+
+
+        image,
+
+
+        categorySelected[id].weight,
+
+
+        qty
+
+
+
+    );
+
+
+
+}
+
+
+
+
+
+
+
+
+// =======================
+// Page Load
+// =======================
+
+
+document.addEventListener(
+"DOMContentLoaded",
+
+function(){
+
+
+
+    renderProducts();
+
+
+
+
+});
---
+++ products-data.js
@@ -0,0 +1,494 @@
+/* ==========================================
+   Khati Shad by Takia
+   products-data.js
+   PART-1
+========================================== */
+
+const products = [
+
+/* ==========================================
+   HONEY
+========================================== */
+
+{
+    id: 1,
+    category: "honey",
+    name: "সুন্দরবনের খাঁটি মধু",
+    image: "images/modhu-+ (2).jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "1 KG",
+            weight: "১ কেজি",
+            price: 1800
+        },
+
+        {
+            label: "500g",
+            weight: "৫০০ গ্রাম",
+            price: 900
+        },
+
+        {
+            label: "250g",
+            weight: "২৫০ গ্রাম",
+            price: 450
+        }
+
+    ]
+
+},
+
+/* ==========================================
+   GHEE
+========================================== */
+
+{
+    id: 2,
+    category: "ghee",
+    name: "দেশি গাওয়া ঘি",
+    image: "images/ghe.offical.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "1 KG",
+            weight: "১ কেজি",
+            price: 1700
+        },
+
+        {
+            label: "500g",
+            weight: "৫০০ গ্রাম",
+            price: 850
+        },
+
+        {
+            label: "250g",
+            weight: "২৫০ গ্রাম",
+            price: 430
+        }
+
+    ]
+
+},
+/* ==========================================
+   OIL
+========================================== */
+
+{
+    id: 7,
+    category: "tel",
+    name: "খাঁটি নারিকেল তেল",
+    image: "images/narikeltel.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 480
+        },
+
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 960
+        },
+
+        {
+            label: "1 LTR",
+            weight: "১ লিটার",
+            price: 1720
+        }
+
+    ]
+
+},
+
+{
+    id: 8,
+    category: "tel",
+    name: "খাঁটি সরিষার তেল",
+    image: "images/sorisertel.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 200
+        },
+
+        {
+            label: "1 LTR",
+            weight: "১ লিটার",
+            price: 350
+        },
+
+        {
+            label: "2 LTR",
+            weight: "২ লিটার",
+            price: 700
+        }
+
+    ]
+
+},
+
+/* ==========================================
+   SPICES
+========================================== */
+
+{
+    id: 3,
+    category: "spices",
+    name: "হলুদ গুঁড়া",
+    image: "images/holud-gura.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "500g",
+            weight: "৫০০ গ্রাম",
+            price: 350
+        },
+
+        {
+            label: "250g",
+            weight: "২৫০ গ্রাম",
+            price: 175
+        },
+
+        {
+            label: "100g",
+            weight: "১০০ গ্রাম",
+            price: 70
+        }
+
+    ]
+
+},
+
+{
+    id: 4,
+    category: "spices",
+    name: "মরিচ গুঁড়া",
+    image: "images/moris-gura.png",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "500g",
+            weight: "৫০০ গ্রাম",
+            price: 350
+        },
+
+        {
+            label: "250g",
+            weight: "২৫০ গ্রাম",
+            price: 175
+        },
+
+        {
+            label: "100g",
+            weight: "১০০ গ্রাম",
+            price: 70
+        }
+
+    ]
+
+},
+
+{
+    id: 5,
+    category: "spices",
+    name: "আতপ চালের গুঁড়া",
+    image: "images/atob-chal.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "3 KG",
+            weight: "৩ কেজি",
+            price: 570
+        },
+
+        {
+            label: "2 KG",
+            weight: "২ কেজি",
+            price: 380
+        },
+
+        {
+            label: "1 KG",
+            weight: "১ কেজি",
+            price: 190
+        }
+
+    ]
+
+},
+
+{
+    id: 16,
+    category: "spices",
+    name: "ঝালমুড়ি মসলা",
+    image: "images/jalmorir-mosla.jpeg",
+    stock: true,
+
+    sizes: [
+
+        {
+            label: "100 GM",
+            weight: "১০০ গ্রাম",
+            price: 129
+        },
+
+        {
+            label: "250 GM",
+            weight: "২৫০ গ্রাম",
+            price: 349
+        },
+
+        {
+            label: "500 GM",
+            weight: "৫০০ গ্রাম",
+            price: 599
+        }
+
+    ]
+
+},
+/* ==========================================
+   PICKLES
+========================================== */
+
+{
+    id: 6,
+    category: "achar",
+    name: "রসুনের আচার",
+    image: "images/rosun-achar.jpeg",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 GM",
+            weight: "৫০০ গ্রাম",
+            price: 440
+        },
+        {
+            label: "250 GM",
+            weight: "২৫০ গ্রাম",
+            price: 220
+        },
+        {
+            label: "100 GM",
+            weight: "১০০ গ্রাম",
+            price: 90
+        }
+    ]
+
+},
+
+{
+    id: 9,
+    category: "achar",
+    name: "জলপাই আচার",
+    image: "images/jolpai-acar.jpeg",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 440
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 220
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 90
+        }
+    ]
+
+},
+
+{
+    id: 10,
+    category: "achar",
+    name: "তেঁতুলের আচার",
+    image: "images/tatuler-acar.png",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 500
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 250
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 100
+        }
+    ]
+
+},
+
+{
+    id: 11,
+    category: "achar",
+    name: "আমলকীর আচার",
+    image: "images/amlokir-acar.png",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 440
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 220
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 90
+        }
+    ]
+
+},
+
+{
+    id: 12,
+    category: "achar",
+    name: "আমড়ার আচার",
+    image: "images/amrar-acar.png",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 420
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 210
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 90
+        }
+    ]
+
+},
+
+{
+    id: 13,
+    category: "achar",
+    name: "চুইঝালের আচার",
+    image: "images/chuijal-acar.png",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 800
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 400
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 160
+        }
+    ]
+
+},
+
+{
+    id: 14,
+    category: "achar",
+    name: "কামরাঙ্গা আচার",
+    image: "images/kamranga-acar.jpeg",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 399
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 199
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 79
+        }
+    ]
+
+},
+
+{
+    id: 15,
+    category: "achar",
+    name: "শুটকির আচার",
+    image: "images/sutkir-acar.jpeg",
+    stock: true,
+
+    sizes: [
+        {
+            label: "500 ML",
+            weight: "৫০০ মিলি",
+            price: 699
+        },
+        {
+            label: "250 ML",
+            weight: "২৫০ মিলি",
+            price: 349
+        },
+        {
+            label: "100 ML",
+            weight: "১০০ মিলি",
+            price: 149
+        }
+    ]
+
+}
+/* ==========================================
+   END OF PRODUCTS
+========================================== */
+
+];
